00bda03aa64e13a74a89919f61bc56ace36f0678
snowShiba <83089711+snowShiba@users.noreply.github.com>
2021-11-29 21:01:23 -0500

    Update install.sh Part 1
    No longer echo's status
    No longer installs workstation or discord-canary
    
    Now uses yay for AUR packages
    Now install man-db, geary and steam
    Now installs custom pacman.conf
    Now installs Nuclear, BitWarden and Etcher (AppImages) and their respective .desktop files
    Now uninstalls bloat packages

--- Part 1/install.sh
+++ Part 1/install.sh
@@ -1,107 +1,105 @@
-echo
-echo Installing git, discord, discord-canary, dkms...
-echo
+# Backup Pacman Config
+sudo cp /etc/pacman.conf /etc/pacman.conf.bak
 
-sudo pacman -S git discord discord-canary dkms
+# Move New Pacman Config
+sudo mv ./configs/pacman.conf /etc/pacman.conf
 
-echo
-echo Installed git, discord, discord-canary, dkms
-echo Installing brave, minecraft-launcher, nordvpn, qview, timeshift, vmware-keymaps, vmware-workstation, linux-headers...
-echo
+# Update System
+sudo pacman --noconfirm -Syu
 
+# Install steam, geary, man, git and discord
+sudo pacman --noconfirm -S git man-db discord geary steam
+
+# Install yay
 cd ~/Downloads/
-git clone https://aur.archlinux.org/brave-bin.git
-cd brave-bin/
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin/
 makepkg -is
 
-git clone https://aur.archlinux.org/minecraft-launcher.git
-cd minecraft-launcher/
-makepkg -is
+# Install Brave, Minecraft, Timeshift, NordVPN and qView
+yay -S brave-bin minecraft-launcher timeshift-bin nordvpn-bin qview
 
-git clone https://aur.archlinux.org/nordvpn-bin.git
-cd nordvpn-bin/
-makepkg -is
+# Enable NordVPN
 sudo systemctl enable --now nordvpnd
 sudo usermod -aG nordvpn $USER
 
-git clone https://aur.archlinux.org/qview.git
-cd qview/
-makepkg -is
-
-git clone https://aur.archlinux.org/timeshift-bin.git
-cd timeshift-bin/
-makepkg -is
-
-git clone https://aur.archlinux.org/vmware-keymaps.git
-cd vmware-keymaps/
-makepkg -is
-
-git clone https://aur.archlinux.org/vmware-workstation.git
-cd vmware-workstation/
-makepkg -is
-
-sudo pacman -S linux-headers
-sudo modprobe -a vmw_vmci vmmon
-
-echo
-echo Installed brave, minecraft-launcher, nordvpn, qview, timeshift, vmware-keymaps, vmware-workstation, linux-headers
-echo Enabling user-theme
-echo
-
+# Enable User Theme and Places Menu Extension
 gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
+gnome-extensions enable places-menu@gnome-shell-extensions.gcampax.github.com
 
-echo
-echo Enabled user-themes
-echo Installing Qogir-theme...
-echo
-
+# Install and Enable Qogir Theme
+cd ~/Downloads/
 git clone https://github.com/vinceliuice/Qogir-theme.git
-cd Qogir-theme
+cd Qogir-theme/
 sudo ./install.sh -l arch -g -c dark --tweaks square -t default
 gsettings set org.gnome.shell.extensions.user-theme name Qogir-dark
 gsettings set org.gnome.desktop.interface gtk-theme Qogir-dark
 
-echo
-echo Installed Qogir-theme
-echo Installing WhiteSur-icon-theme...
-echo
-
+# Install and Enable WhiteSur Icon Theme
+cd ~/Downloads/
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme
 cd WhiteSur-icon-theme/
 ./install.sh -b
 gsettings set org.gnome.desktop.interface icon-theme WhiteSur
 
-echo
-echo Installed WhiteSur-icon-theme
-echo Enabling places-menu
-echo
-
-gnome-extensions enable places-menu@gnome-shell-extensions.gcampax.github.com
-
-echo
-echo Enabled places-menu
-echo Installing dash-to-dock, alphabetical-grid-extension, just-perfection, tray-icons-reloaded...
-echo
-
+# Install Dash to Dock Extension
+cd ~/Downloads/
 git clone https://github.com/micheleg/dash-to-dock.git
 cd dash-to-dock/
 make
 make install
 
+# Install Alphabetical Grid Extension
+cd ~/Downloads/
 git clone https://github.com/stuarthayhurst/alphabetical-grid-extension.git
 cd alphabetical-grid-extension/
 make build
 make install
 
+# Install Just Perfection Extension
+cd ~/Downloads/
 git clone https://gitlab.gnome.org/jrahmatzadeh/just-perfection.git
 cd just-perfection/
 ./scripts/build.sh -i
 
+# Install Tray Icons Reloaded Extension
 cd ~/.local/share/gnome-shell/extensions/
 git clone https://github.com/MartinPL/Tray-Icons-Reloaded.git
 mv ./Tray-Icons-Reloaded ./trayIconsReloaded@selfmade.pl
 
+# Install Nuclear v0.6.16
+sudo mkdir -p /opt/.AppImages/Nuclear/
+cd /opt/.AppImages/Nuclear/
+sudo wget "https://raw.githubusercontent.com/nukeop/nuclear/master/build/icons/512x512.png" -O Nuclear.png
+sudo wget "https://github.com/nukeop/nuclear/releases/download/v0.6.16/nuclear-v0.6.16.AppImage" -O Nuclear.AppImage
+sudo chmod +x Nuclear.AppImage
+cd ~/Downloads/snowshiba-arch-reinstall-tools/part\ 1/desktop\ files/
+chmod +x nuclear.desktop
+sudo desktop-file-install nuclear.desktop
+
+# Install BitWarden
+sudo mkdir -p /opt/.AppImages/BitWarden
+cd /opt/.AppImages/BitWarden
+sudo wget "https://avatars.githubusercontent.com/u/15990069?s=200&v=4" -O BitWarden.png
+sudo wget "https://github.com/bitwarden/desktop/releases/download/v1.29.1/Bitwarden-1.29.1-x86_64.AppImage" -O BitWarden.AppImage
+sudo chmod +x BitWarden.AppImage
+cd ~/Downloads/snowshiba-arch-reinstall-tools/part\ 1/desktop\ files/
+chmod +x bitwarden.desktop
+sudo desktop-file-install bitwarden.desktop
+
+# Install Etcher
+sudo mkdir -p /opt/.AppImages/Etcher
+cd /opt/.AppImages/Etcher
+sudo wget "https://raw.githubusercontent.com/balena-io/etcher/master/assets/iconset/512x512.png" -O Etcher.png
+sudo wget "https://github.com/balena-io/etcher/releases/download/v1.7.1/balenaEtcher-1.7.1-x64.AppImage" -O Etcher.AppImage
+sudo chmod +x Etcher.AppImage
+cd ~/Downloads/snowshiba-arch-reinstall-tools/part\ 1/desktop\ files/
+chmod +x etcher.desktop
+sudo desktop-file-install etcher.desktop
+
+# Uninstall Bloat
+sudo pacman --noconfirm -Rs gnome-contacts gnome-clocks gnome-maps gnome-books gnome-photos gnome-boxes cheese htop vim
+
 echo
-echo Installed dash-to-dock, alphabetical-grid-extension, just-perfection, tray-icons-reloaded
-echo Part 1 complete! Please reboot and run the installer script in the Part 2 directory!
+echo Part 1 complete! Please reboot and run install.sh in the Part 2 directory!
 echo
